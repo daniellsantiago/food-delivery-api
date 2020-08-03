f947176c22e3532cc40c60baf728f26266211a5e
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-03 17:32:10 -0300

    Set the project time zone to UTC

--- src/main/java/com/daniellsantiago/fooddeliveryapi/FoodDeliveryApiApplication.java
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/FoodDeliveryApiApplication.java
@@ -3,10 +3,13 @@ package com.daniellsantiago.fooddeliveryapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FoodDeliveryApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(FoodDeliveryApiApplication.class, args);
 	}
 
