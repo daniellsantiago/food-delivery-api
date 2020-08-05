4882b7958950e13dc54a8b6e6aa3907b32ce75af
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-04 22:02:12 -0300

    Support methods that requires Pageable

---
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/api/openapi/model/CuisinesModelOpenApi.java
@@ -0,0 +1,9 @@
+package com.daniellsantiago.fooddeliveryapi.api.openapi.model;
+
+import com.daniellsantiago.fooddeliveryapi.api.dto.CuisineDTO;
+import io.swagger.annotations.ApiModel;
+
+@ApiModel("Cuisine Page Model")
+public class CuisinesModelOpenApi extends PagedModelOpenApi<CuisineDTO>{
+
+}
---
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/api/openapi/model/OrderBasicModelOpenApi.java
@@ -0,0 +1,8 @@
+package com.daniellsantiago.fooddeliveryapi.api.openapi.model;
+
+import com.daniellsantiago.fooddeliveryapi.api.dto.OrderBasicDTO;
+import io.swagger.annotations.ApiModel;
+
+@ApiModel("Order Page Model")
+public class OrderBasicModelOpenApi extends PagedModelOpenApi<OrderBasicDTO>{
+}
