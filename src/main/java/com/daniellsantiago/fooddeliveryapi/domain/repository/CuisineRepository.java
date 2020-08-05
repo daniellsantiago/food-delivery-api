5e80b164343010186626f5311af0920402699f25
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-04 21:49:36 -0300

    Added findAll with Pageable to CuisineRepository

--- src/main/java/com/daniellsantiago/fooddeliveryapi/domain/repository/CuisineRepository.java
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/domain/repository/CuisineRepository.java
@@ -1,6 +1,8 @@
 package com.daniellsantiago.fooddeliveryapi.domain.repository;
 
 import com.daniellsantiago.fooddeliveryapi.domain.model.Cuisine;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,5 +11,6 @@ public interface CuisineRepository {
     Cuisine save(Cuisine entity);
     void deleteById(Long id);
     List<Cuisine> findAll();
+    Page<Cuisine> findAll(Pageable pageable);
     Optional<Cuisine> findById(Long id);
 }
