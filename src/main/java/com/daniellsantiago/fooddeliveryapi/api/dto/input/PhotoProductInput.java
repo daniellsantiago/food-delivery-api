5b08674c18bda40e0ae52e51c15cbf75c3994afd
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-08 15:07:25 -0300

    Hide file parameter at the swagger

--- src/main/java/com/daniellsantiago/fooddeliveryapi/api/dto/input/PhotoProductInput.java
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/api/dto/input/PhotoProductInput.java
@@ -17,14 +17,14 @@ import javax.validation.constraints.NotNull;
 @ApiModel("Photo Product Input Model")
 public class PhotoProductInput {
 
-    @ApiModelProperty(example = "photo.png", required = true)
+    @ApiModelProperty(hidden = true)
     @NotNull
     @FileSize(max = "500KB", message = "The photo must have a maximum size of {max}")
     @FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE },
                     message = "The photo type must be {allowed}")
     private MultipartFile file;
 
-    @ApiModelProperty(example = "Photo describing my house", required = true)
+    @ApiModelProperty(value = "Product photo description", required = true)
     @NotBlank
     private String description;
 }
