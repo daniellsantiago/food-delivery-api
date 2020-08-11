2849ed04f5e2dbb8b65fc931fbd11a3b825a49ab
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-11 10:27:42 -0300

    Added to Order swagger doc support to Hateoas links

--- src/main/java/com/daniellsantiago/fooddeliveryapi/api/openapi/model/OrderBasicModelOpenApi.java
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/api/openapi/model/OrderBasicModelOpenApi.java
@@ -2,7 +2,27 @@ package com.daniellsantiago.fooddeliveryapi.api.openapi.model;
 
 import com.daniellsantiago.fooddeliveryapi.api.dto.OrderBasicDTO;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
 
-@ApiModel("Order Page Model")
-public class OrderBasicModelOpenApi extends PagedModelOpenApi<OrderBasicDTO>{
+import java.util.List;
+
+@ApiModel("OrderBasicModel")
+@Setter
+@Getter
+public class OrderBasicModelOpenApi {
+
+    private OrderBasicEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
+
+    @ApiModel("OrderBasicEmbeddedModel")
+    @Data
+    public class OrderBasicEmbeddedModelOpenApi {
+
+        private List<OrderBasicDTO> orders;
+
+    }
 }
