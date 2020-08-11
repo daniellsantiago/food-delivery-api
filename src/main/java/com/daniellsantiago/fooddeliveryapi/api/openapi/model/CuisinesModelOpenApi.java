18c6ced24cb2a96e207e0bb5e542011b51e2fc0b
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-11 10:25:46 -0300

    Added to Cuisine swagger doc support to Hateoas links

--- src/main/java/com/daniellsantiago/fooddeliveryapi/api/openapi/model/CuisinesModelOpenApi.java
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/api/openapi/model/CuisinesModelOpenApi.java
@@ -2,8 +2,27 @@ package com.daniellsantiago.fooddeliveryapi.api.openapi.model;
 
 import com.daniellsantiago.fooddeliveryapi.api.dto.CuisineDTO;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
 
-@ApiModel("Cuisine Page Model")
-public class CuisinesModelOpenApi extends PagedModelOpenApi<CuisineDTO>{
+import java.util.List;
 
+@ApiModel("CuisinesModel")
+@Setter
+@Getter
+public class CuisinesModelOpenApi{
+
+    private CuisinesEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
+
+    @ApiModel("CuisinesEmbeddedModel")
+    @Data
+    public class CuisinesEmbeddedModelOpenApi {
+
+        private List<CuisineDTO> cuisines;
+
+    }
 }
