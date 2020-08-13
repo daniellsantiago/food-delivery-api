d3fd0451c5c1b54a7cfa6c6f97bbd1499e7afc5a
Daniel Santiago <danielsantiago111@hotmail.com>
2020-08-13 19:00:25 -0300

    Configure Spring Security

---
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/core/security/ApplicationSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.daniellsantiago.fooddeliveryapi.core.security;
+
+import com.daniellsantiago.fooddeliveryapi.core.security.auth.AuthUserService;
+import com.daniellsantiago.fooddeliveryapi.core.security.jwt.JwtConfig;
+import com.daniellsantiago.fooddeliveryapi.core.security.jwt.JwtTokenVerifier;
+import com.daniellsantiago.fooddeliveryapi.core.security.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final AuthUserService authUserService;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/**")
+                .authenticated();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(authUserService);
+        return provider;
+    }
+}
