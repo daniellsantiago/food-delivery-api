43fc7b82be8452809e1cde8d71e1c98d47d8d1f6
Daniel Santiago <danielsantiago111@hotmail.com>
2020-09-21 18:19:03 -0300

    Useless import

--- src/main/java/com/daniellsantiago/fooddeliveryapi/api/assembler/disassembler/UserInputDisassembler.java
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/api/assembler/disassembler/UserInputDisassembler.java
@@ -1,11 +1,9 @@
 package com.daniellsantiago.fooddeliveryapi.api.assembler.disassembler;
 
 import com.daniellsantiago.fooddeliveryapi.api.dto.input.UserInput;
-import com.daniellsantiago.fooddeliveryapi.domain.exception.InvalidDataRequestException;
 import com.daniellsantiago.fooddeliveryapi.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
 @Component
