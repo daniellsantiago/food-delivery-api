e42a36a02fa8141c661bee37e83d0c00a3adddb0
Daniel Santiago <danielsantiago111@hotmail.com>
2020-09-21 18:18:53 -0300

    Useless import

--- src/main/java/com/daniellsantiago/fooddeliveryapi/core/web/WebConfig.java
+++ src/main/java/com/daniellsantiago/fooddeliveryapi/core/web/WebConfig.java
@@ -3,7 +3,6 @@ package com.daniellsantiago.fooddeliveryapi.core.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
